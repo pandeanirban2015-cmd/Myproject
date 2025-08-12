1376cb96f3f01d3d206b3494a2785aa98e793251
pandeanirban2015-cmd <pandeanirban2015@gmail.com>
2025-08-12 12:00:18 +0530

    I have changed if and my name and put if and else

--- main.ts
+++ main.ts
@@ -2,6 +2,20 @@ let Hand = 0
 input.onGesture(Gesture.Shake, function () {
     Hand = randint(0, 10)
     if (Hand == 6) {
-        basic.showString("Aniket")
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
     }
 })
